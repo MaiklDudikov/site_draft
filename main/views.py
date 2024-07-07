3f379ef13ebfd471075eba28f23cdb189dc135a0
MaiklDudikov <wmiwa87@gmail.com>
2024-07-07 17:10:59 +0200

    edited files profile, register, view

--- main/views.py
+++ main/views.py
@@ -3,28 +3,6 @@ from .models import User
 from .forms import UserForm
 
 
-def list_users(request):
-    users_psw = User.objects.all()
-    return render(request, 'main/list_users.html', {'user' : users_psw})
-
-
-def profile(request):
-    name = request.POST.get("name", "Undefined")
-    return render(request, 'main/profile.html', {'user' : name})
-
-
-def login(request):
-    return render(request, 'main/login.html')
-
-
-def register(request):
-    return render(request, 'main/register.html')
-
-
-def video(request):
-    return render(request, 'main/video.html')
-
-
 def main_register(request):
     error = ''
     if request.method == 'POST':
@@ -41,3 +19,45 @@ def main_register(request):
     }
 
     return render(request, 'main/register.html', data)
+
+
+def list_users(request):
+    users_psw = User.objects.all()
+    return render(request, 'main/list_users.html', {'user' : users_psw})
+
+
+# "Undefined"
+# @login_required
+# {'user': request.user}
+def profile(request):
+    name = request.POST.get("name", "Undefined")
+    # email = request.POST.get('email')
+    # password = request.POST.get('password')
+    return render(request, 'main/profile.html', {'user' : name})
+
+
+def login(request):
+    return render(request, 'main/login.html')
+
+
+# def login(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         password = request.POST.get('password')
+#         # user = authenticate(request, name=name, password=password)
+#         if user is not None:
+#             # auth_login(request, user)
+#             return redirect('profile')
+#         else:
+#             error = 'Такого пользователя не существует'
+#             return render(request, 'main/login.html', {'error': error})
+
+#     return render(request, 'main/login.html')
+
+
+def register(request):
+    return render(request, 'main/register.html')
+
+
+def video(request):
+    return render(request, 'main/video.html')
